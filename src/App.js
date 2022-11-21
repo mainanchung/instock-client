5203d8cfc19124caf74d20624a371c1f803c9d3c
Tyalor <tyalorkny@gmail.com>
2022-11-21 16:36:30 -0500

    enter front end

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route
             path = '/inventory'
