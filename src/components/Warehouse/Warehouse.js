a26a816eeb65a0c5c5b12b2336eca23bed4025bc
Tlau425 <tlau425@yahoo.com>
2022-11-22 14:08:34 -0500

    setting up warehouse

--- src/components/Warehouse/Warehouse.js
+++ src/components/Warehouse/Warehouse.js
@@ -1,14 +1,30 @@
 import './warehouse.scss'
 import backIcon from '../../assets/Icons/arrow_back-24px.svg'
 import editIcon from '../../assets/Icons/edit-24px.svg'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState} from 'react';
 
 function Warehouse() {
+    const {warehouseId} = useParams()
+    const [warehouse, setWarehouse] = useState('')
+    const getWarehouse = () => {
+        axios.get(`http://localhost:8080/warehouse/${warehouseId}`)
+        .then((response) => {
+            setWarehouse(response.data)
+        })
+    }
+
+    useEffect(() => {
+        getWarehouse()
+
+    }, [])
     return (
         <div className="warehouse">
             <div className="warehouse__title-container">
                 <div className="warehouse__icon-title">
                     <img src={backIcon} />
-                    <h2>Washington</h2>
+                    <h2>{warehouse}</h2>
                 </div>
                 <button className="warehouse__edit-button"><img src={editIcon} /></button>
             </div>
