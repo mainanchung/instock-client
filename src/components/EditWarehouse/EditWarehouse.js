9247483260f1bb4bfa94e5dcda6c7b6de2e558b4
Tyalor <tyalorkny@gmail.com>
2022-11-22 14:47:22 -0500

    edit warehouse component work in progress

--- src/components/EditWarehouse/EditWarehouse.js
+++ src/components/EditWarehouse/EditWarehouse.js
@@ -5,37 +5,53 @@ const EditWarehouse = () => {
     return (
         <div className="edit-warehouse">
             <form className="edit-warehouse__form">
-                <div className="edit-warehouse__container">
-                    <img className="edit-warehouse__img" src={ArrowBack} alt="arrow-back" />
+                <div className="edit-warehouse__heading-container">
+                    <img className="edit-warehouse__heading-img" src={ArrowBack} alt="arrow-back" />
                     <h1 className="edit-warehouse__heading">Edit Warehouse</h1>
                 </div>
                 <div className="edit-warehouse__content-container">
                     <hr className="edit-warehouse__hr" />
                     <div className="edit-warehouse__content">
                         <h3 className="edit-warehouse__subheading">Warehouse Details</h3>
-                        <div className="edit-warehouse__item">
-                            <label className="edit-warehouse__label" htmlFor="">Warehouse Name</label>
-                            <input className="edit-warehouse__input" type="text" placeholder="ex. Washington"/>
-                            <label className="edit-warehouse__label" htmlFor="">Street Address</label>
-                            <input className="edit-warehouse__input" type="text" placeholder="ex. 33 Pearl Street SW"/>
-                            <label className="edit-warehouse__label" htmlFor="">City</label>
-                            <input className="edit-warehouse__input" type="text" placeholder="ex. Washington"/>
-                            <label className="edit-warehouse__label" htmlFor="">Country</label>
-                            <input className="edit-warehouse__input" type="text" placeholder="ex. USA"/>
+                        <div className="edit-warehouse__form-list">
+                            <div className="edit-warehouse__form-item">
+                                <label className="edit-warehouse__label" htmlFor="">Warehouse Name</label>
+                                <input className="edit-warehouse__input" type="text" placeholder="ex. Washington"/>
+                            </div>
+                            <div className="edit-warehouse__form-item">
+                                <label className="edit-warehouse__label" htmlFor="">Street Address</label>
+                                <input className="edit-warehouse__input" type="text" placeholder="ex. 33 Pearl Street SW"/>
+                            </div>
+                            <div className="edit-warehouse__form-item">
+                                <label className="edit-warehouse__label" htmlFor="">City</label>
+                                <input className="edit-warehouse__input" type="text" placeholder="ex. Washington"/>
+                            </div>
+                            <div className="edit-warehouse__form-item">
+                                <label className="edit-warehouse__label" htmlFor="">Country</label>
+                                <input className="edit-warehouse__input" type="text" placeholder="ex. USA"/>
+                            </div>
                         </div>
                     </div>
                     <hr className="edit-warehouse__hr"/>
                     <div className="edit-warehouse__content">
                         <h3 className="edit-warehouse__subheading">Contact Details</h3>
                         <div className="edit-warehouse__item">
+                        <div className="edit-warehouse__form-item">
                             <label className="edit-warehouse__label" htmlFor="">Contact Name</label>
-                            <input className="edit-warehouse__input" type="text" />
+                            <input className="edit-warehouse__input" type="text" placeholder="ex. Graeme Lyon"/>
+                        </div>
+                        <div className="edit-warehouse__form-item">
                             <label className="edit-warehouse__label" htmlFor="">Position</label>
-                            <input className="edit-warehouse__input" type="text" />
+                            <input className="edit-warehouse__input" type="text" placeholder="ex. Warehouse Manager"/>
+                        </div>
+                        <div className="edit-warehouse__form-item">
                             <label className="edit-warehouse__label" htmlFor="">Phone Number</label>
-                            <input className="edit-warehouse__input" type="text" />
+                            <input className="edit-warehouse__input" type="text" placeholder="ex. +1 (647) 504-0911"/>
+                        </div>
+                        <div className="edit-warehouse__form-item">
                             <label className="edit-warehouse__label" htmlFor="">Email</label>
-                            <input className="edit-warehouse__input" type="text" />
+                            <input className="edit-warehouse__input" type="text" placeholder="ex. glyon@instock.com"/>
+                        </div>
                         </div>
                     </div>
                     <div className="edit-warehouse__btn-container">
