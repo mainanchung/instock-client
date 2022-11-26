4181774392d5c953837dfc951a016f7cb0ab4b37
Tlau425 <tlau425@yahoo.com>
2022-11-26 13:10:25 -0500

    responsive delete modal

--- src/components/DeleteModal/DeleteModal.js
+++ src/components/DeleteModal/DeleteModal.js
@@ -1,23 +1,31 @@
 import './DeleteModal.scss'
 import errorIcon from '../../Assets/Icons/close-24px.svg'
 
-function DeleteModal(){
+function DeleteModal() {
+
+    const deleteHandler = (e) => {
+        e.preventDefault()
+        console.log("y u delete me 4")
+    }
 
     return (
-        <div>
-            <button className="button__close"><img src={errorIcon}/></button>
-            <div>
-            <h2>Delete television inventory item?</h2>
-            <p>Please confirm that you'd like to delete $television
-                from the inventory list. You won't be able to undo this action.
-            </p>
-            </div>
-            <div>
-                <button className="button__cancel">Cancel</button>
-                <button className="button__delete">Delete</button>
+        <div className="delete">
+            <div className="delete-container">
+                <button className="button__close"><img src={errorIcon} alt="error icon" /></button>
+                <div className="delete__header-container">
+                    <h2 className="delete__header">Delete television inventory item?</h2>
+                    <p className="delete__description">Please confirm that you'd like to delete $television
+                        from the inventory list. You won't be able to undo this action.
+                    </p>
+                    <div className="button__container">
+                        <button className="button__cancel">Cancel</button>
+                        <button onSubmit={deleteHandler} className="button__delete">Delete</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
 }
 
-export {DeleteModal}
+export { DeleteModal }
